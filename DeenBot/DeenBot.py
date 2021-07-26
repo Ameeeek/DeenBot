acdbcb31bb3554b26db17b94fa7125b57b736597
Ameeeek <83535916+Ameeeek@users.noreply.github.com>
2021-07-26 13:38:17 +0800

    DeenBot 0.1.2
    What's new: Wordlist

--- DeenBot/DeenBot.py
+++ DeenBot/DeenBot.py
@@ -6,9 +6,10 @@ import json
 import random
 client = discord.Client()
 
-sedih = ["sedih", "stress", "depresi"]
+sedih = ["sedih", "stress", "depresi", "pusing"]
 janganSedih = ["Janganlah kamu bersedih, sesungguhnya Allah bersama kita.— At-Taubah: 40",
-    "Ingatlah, hanya dengan mengingat Allah-lah hati menjadi tenteram.– QS Ar Ra’d: 28",
+    "Ingatlah, hanya dengan mengingat Allah-lah hati menjadi tenteram.– QS Ar Ra’d: 28", 
+    "Ya Allah, aku memohon perlindungan denganMu daripada keluh kesah dan kesedihan, rasa lemah dan kemalasan Aku memohon perlindungan denganMu daripada sifat penakut dan kebakhilam dan aku mohon perlindungan denganMu dari bebanan hutang dan dikuasai seseorang (Hadith riwayat Abu Daud)"
         ]
 
 @client.event
